23411a015056c3be18a75eace6ca89226e262243
thangtm <thangtm589@gmail.com>
2019-12-24 11:52:10 +0700

    components and semantic.min.css

--- src/index.js
+++ src/index.js
@@ -1,32 +1,10 @@
-// Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// function getButtonText() {
-//     return 'Click on me!';
-// }
-
-// Create a react component 
 const App = () => {
-    // const buttonText = 'Click Me!';
-    const buttonText = {text: 'Click Me' };
-    const labelText = 'Enter name:' ;
-
-
-    return (
-        <div>
-            <label className="label" htmlFor="name">
-                {labelText}
-            </label>
-            <input id="name" type="text"/>
-            <button style={{backgroundColor: "blue", color: "white"}}>
-                {/* {buttonText} */}
-                {buttonText.text}
-                {/* {getButtonText()} */}
-            </button>
-        </div>
-    );
+    return <div>Hi there!</div>
 };
 
-// Take the react component and show it on the screen 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+    <App />, document.querySelector('#root')
+);
