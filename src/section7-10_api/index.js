1dd949a690cc06a0079bb39deaf2e965678b7a75
thangtm <thangtm589@gmail.com>
2020-01-07 09:20:40 +0700

    section 7-10 api

--- src/section7-10_api/index.js
+++ src/section7-10_api/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-)
+// ReactDOM.render(
+//     <App />,
+//     document.querySelector('#root')
+// )
